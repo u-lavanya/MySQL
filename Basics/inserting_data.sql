e65d4e047357d376d85d5da4d088646d5ca7cda6
Lavanya U <125444062+u-lavanya@users.noreply.github.com>
2024-09-03 18:47:36 +0530

    Update inserting_data.sql

--- Basics/inserting_data.sql
+++ Basics/inserting_data.sql
@@ -1,1 +1,9 @@
+--we can insert value this way
+/*
+syntax 
+INSERT INTO <tablename>(columnname1,columnname2) VALUES (value1,value2);
+*/
+INSERT INTO cats(name,age) VALUES('cherry' , 2);
 
+-- we can view the table content 
+SELECT * FROM cats;

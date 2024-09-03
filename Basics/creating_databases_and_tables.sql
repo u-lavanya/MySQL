68b3fed496e8035343487e5e5ccbdfcb10a4c520
Lavanya U <125444062+u-lavanya@users.noreply.github.com>
2024-09-03 13:28:40 +0530

    Update creating_databases_and_tables.sql

--- Basics/creating_databases_and_tables.sql
+++ Basics/creating_databases_and_tables.sql
@@ -15,3 +15,7 @@ USE <database_name>;
 
 --Select the database to view which data base is in use
 SELECT DATABASE();
+
+--A database is really just a bunch of tables.
+--They describe the format and the shape of our data and then they hold collections of data that follows that shape.
+-- table is a collection of related data held in a structured format within a database.

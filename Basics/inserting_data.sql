9f2257d20e533f42e68dfc00ef9cafac18f465b8
Lavanya U <125444062+u-lavanya@users.noreply.github.com>
2024-09-05 09:04:45 +0530

    Update inserting_data.sql

--- Basics/inserting_data.sql
+++ Basics/inserting_data.sql
@@ -8,3 +8,25 @@ INSERT INTO cats(name,age) VALUES('cherry' , 2);
 -- we can view the table content 
 SELECT * FROM cats;
 
+-- Single insert (switching order of name and age)
+
+INSERT INTO cats (age, name) 
+VALUES 
+  (2, 'Beth');
+
+
+-- Multiple Insert:
+
+INSERT INTO cats (name, age) 
+VALUES 
+  ('Meatball', 5), 
+  ('Turkey', 1), 
+  ('Potato Face', 15);
+
+EXCERCISE
+
+CREATE TABLE people(
+  first_name varchar(20),
+  last_name varchar(20),
+  age int
+);

bc69881318bb5ea3a614640f2a372d93d378d6b0
Lavanya U <125444062+u-lavanya@users.noreply.github.com>
2024-09-05 10:15:40 +0530

    Update inserting_data.sql

--- Basics/inserting_data.sql
+++ Basics/inserting_data.sql
@@ -54,3 +54,44 @@ SELECT * FROM people;
 DROP TABLE people;
 
 SHOW TABLES;
+
+--not null
+
+/*while describing the table if the null column for the fields are yes that means null value is allowed
+then we can make use of not null*/
+
+--Using NOT NULL:
+
+CREATE TABLE cats2 (
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+--to insert name like MARIO's which has 's we use \' 
+--INSERT INTO cats(name,age) values('Mario\'s' , 12);
+
+--Define a table with a DEFAULT name specified:
+
+CREATE TABLE cats3  (    
+    name VARCHAR(20) DEFAULT 'no name provided',    
+    age INT DEFAULT 99  
+);
+
+--Notice the change when you describe the table:
+
+DESC cats3;
+
+--Insert a cat without a name:
+
+INSERT INTO cats3(age) VALUES(13);
+
+--Or a nameless, ageless cat:
+
+INSERT INTO cats3() VALUES();
+
+--Combine NOT NULL and DEFAULT:
+
+CREATE TABLE cats4  (    
+    name VARCHAR(20) NOT NULL DEFAULT 'unnamed',    
+    age INT NOT NULL DEFAULT 99 
+);
